4c5e8ff4106253e3fc2c14842e6cb10831edd6ba
mindie-wellness <mindiewellness@gmail.com>
2025-11-02 07:16:34 +0530

    Delete selfcare-mvp directory

--- selfcare-mvp/pages/api/tips.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { supabaseAdmin } from './_supabase'
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const sb = supabaseAdmin()
-  const { data, error } = await sb.from('tips').select('*').order('created_at', { ascending: false }).limit(50)
-  if (error) return res.status(500).json({ error: error.message })
-  return res.json({ items: data })
-}
